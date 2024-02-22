6e04671e3eb1c2ad91c911ed4549cfce4041cda4
Andy <andyfartushniy@gmail.com>
2024-02-22 16:30:30 +0200

    Added routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,17 @@
-import { Routes } from "react-router-dom"
+import Cataloge from "pages/Cataloge";
+import Favorite from "pages/Favorite";
+import HomePage from "pages/HomePage";
+import { Route, Routes } from "react-router"
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cataloge" element={<Cataloge />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        ♥☻Rent Car Website☻♥
-      </div>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
+import Header from "components/Heder/Heder";
+
 const HomePage = () => {
-    
+    return (
+      <>
+        <Header />
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          ♥☻Rent Car Website☻♥
+        </div>
+      </>
+    );
 };
 
 export default HomePage;
