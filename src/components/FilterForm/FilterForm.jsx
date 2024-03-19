e2dd0c786990ef94e7e3f0fc09744f095115aa12
Andy <andyfartushniy@gmail.com>
2024-03-19 23:12:22 +0200

    Worked with cards' filter

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -1,7 +1,7 @@
 import { Formik } from "formik";
 import makes from '../../makes.json';
 import * as yup from 'yup';
-import { StyledField, StyledForm } from "./FilterForm.styled";
+import { FilterButton, FormBlock, MileageForm, StyledField, StyledForm } from "./FilterForm.styled";
 
 const schema = yup.object().shape({
   carBrand: yup
@@ -19,12 +19,37 @@ const FilterForm = () => {
         validationSchema={schema}
       >
         <StyledForm>
-          <label>Car brand</label>
-          <StyledField as="select" name="carBrand">
-            {makes.map(make => (
-              <option value="Enter the text">{make}</option>
-            ))}
-          </StyledField>
+          <FormBlock>
+            <label>Car brand</label>
+            <StyledField as="select" placeholder="carBrand">
+              {makes.map(make => (
+                <option value="Enter the text">{make}</option>
+              ))}
+            </StyledField>
+          </FormBlock>
+          <FormBlock>
+            <label>Price/1 hour</label>
+            <StyledField as="select" name="price">
+              <option value="To $">10</option>
+              <option value="To $">20</option>
+              <option value="To $">30</option>
+              <option value="To $">40</option>
+              <option value="To $">50</option>
+              <option value="To $">60</option>
+              <option value="To $">70</option>
+              <option value="To $">80</option>
+              <option value="To $">90</option>
+              <option value="To $">100</option>
+            </StyledField>
+          </FormBlock>
+          <FormBlock>
+            <label>Car mileage/km</label>
+            <MileageForm>
+              <StyledField type="text" name="name" />
+              <StyledField type="text" name="name" />
+            </MileageForm>
+          </FormBlock>
+          <FilterButton>Search</FilterButton>
         </StyledForm>
       </Formik>
     );
--- src/components/FilterForm/FilterForm.styled.js
+++ src/components/FilterForm/FilterForm.styled.js
@@ -2,11 +2,60 @@ import { Field, Form } from "formik";
 import styled from "styled-components";
 
 export const StyledForm = styled(Form)`
-    width: 300px;
-    margin-left: auto;
-    margin-right: auto;
+    display: flex;
+    justify-content: center;
+    gap: 18px;
+    margin-top: 50px;
+`;
+
+export const FormBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 129%;
+    color: #8a8a89;
+    /* width: 300px; */
+`;
+
+export const MileageForm = styled.div`
+    display: inline-block;
 `;
 
 export const StyledField = styled(Field)`
-    width: 300px;
+    border-radius: 14px;
+    border: 0;
+    margin-top: 8px;
+    padding: 14px 89px 14px 18px;
+    width: 224px;
+    height: 48px;
+    background: #f7f7fb;
+`;
+
+export const PriceField = styled(Field)`
+    border-radius: 14px;
+    border: 0;
+    margin-top: 8px;
+    padding: 14px 89px 14px 18px;
+    width: 125px;
+    height: 48px;
+    background: #f7f7fb;
+`;
+
+export const FilterButton = styled.button`
+    align-self: flex-end;
+    border-radius: 12px;
+    border: 0;
+    padding: 14px 44px;
+    width: 136px;
+    height: 48px;
+    background: #3470ff;
+    cursor: pointer;
+
+    font-size: 14px;
+    line-height: 143%;
+    color: #fff;
+
+    &:hover{background-color: #0B44CD;}
 `;
